e72c2a4b8d0e39274d2d2cf42e91a8da14f180a4
xjenek <xxjenekxx@gmail.com>
2019-03-04 14:36:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const core = require('biot-core');
+const eventBus = require('ocore/event_bus');
+const fs = require('fs');
+
+let addresses = {};
+try {
+	fs.accessSync('./attested', fs.constants.R_OK | fs.constants.W_OK);
+	addresses = fs.readFileSync('./attested');
+	addresses = JSON.parse(addresses);
+} catch (e) {
+	addresses = {};
+}
+
+(async () => {
+	await core.init('test');
+	
+	eventBus.on('object', async (from_address, object) => {
+		if (object.app === 'BIoT') {
+			if (object.type === 'hello') {
+				if (addresses[from_address]) {
+					core.sendTechMessageToDevice(from_address, {
+						type: 'render', page: 'attested', form: [
+							{type: 'h2', title: 'You attested'}
+						]
+					});
+				} else {
+					core.sendTechMessageToDevice(from_address, {
+						type: 'render', page: 'index', form: [
+							{type: 'input', title: 'Name', id: 'name', required: true},
+							{type: 'input', title: 'Last Name', id: 'lname', required: true},
+							{type: 'input', title: 'Age', id: 'age', required: true},
+							{type: 'address', required: true, title: 'Select wallet for address', id: 'address'},
+							{type: 'blank_line'},
+							{type: 'submit', title: 'Send'}
+						]
+					});
+				}
+			} else if (object.type === 'response') {
+				console.error('resp', object);
+				if (object.page === 'index') {
+					let res = await core.postPrivateProfile(object.response.address, {
+						name: object.response.name,
+						lname: object.response.lname,
+						age: object.response.age
+					});
+					console.error(res);
+					core.sendTechMessageToDevice(from_address, {
+						type: 'addProfile',
+						my_address: res.address,
+						your_address: object.response.address,
+						unit: res.objJoint.unit.unit,
+						profile: res.src_profile
+					});
+					addresses[from_address] = true;
+					fs.writeFileSync('./attested', JSON.stringify(addresses));
+					setTimeout(() => {
+						core.sendTechMessageToDevice(from_address, {
+							type: 'render', page: 'attested', form: [
+								{type: 'h2', title: 'You attested'}
+							]
+						});
+					}, 200);
+				}
+			}
+		}
+	});
+	
+})().catch(console.error);
